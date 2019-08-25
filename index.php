2dd1e659237cf836cc091b7a2751ee8a762c045a
hudsonchromy <hjc0018@auburn.edu>
2019-08-25 15:13:38 -0500

    make it better

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML>
+	<head>
+		<title>ACM Attendance</title>
+	</head>
+	<body>
+		<form action="index.php" method="post">
+			<select name="club" size="4" multiple>
+			  <option value="volvo">EHC</option>
+			  <option value="saab">CPT</option>
+			  <option value="fiat">AI</option>
+			  <option value="audi">Other</option>
+			</select>
+		</form>
+	</body>
+</HTML>
