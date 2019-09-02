49234c51915de587ea7485538af8c1859cdb0c23
hudsonchromy <hjc0018@auburn.edu>
2019-09-02 16:18:14 -0500

    working

--- server.php
+++ server.php
@@ -1,1 +1,15 @@
-server.php
+<?
+session_start();
+if (isset($_POST['eventSelect'])) {
+	$_SESSION['event'] = $_POST['club'];
+	header('location: index.php');
+}
+if (isset($_POST['login'])) {
+	$club = $_SESSION['event'];
+	$db = mysqli_connect('127.0.0.1', 'root', '12345', 'attendance') or die('Error: Unable to Connect');
+	$username = mysqli_real_escape_string($db, $_POST['username']);
+	$times = microtime(true);
+	$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
+	mysqli_query($db, $sql);
+}
+?>
