ecf000f01ffdd7330b13356443bb42ae11617bce
hudsonchromy <hjc0018@auburn.edu>
2019-09-03 16:07:58 -0500

    added env variables

--- server.php
+++ server.php
@@ -6,7 +6,11 @@ if (isset($_POST['eventSelect'])) {
 }
 if (isset($_POST['login'])) {
 	$club = $_SESSION['event'];
-	$db = mysqli_connect('127.0.0.1', 'root', '12345', 'attendance') or die('Error: Unable to Connect');
+	$HOST = getenv('host');
+		$USERNAME = getenv('username');
+		$PASSWORD = getenv('password');
+		$DBNAME = getenv('dbname');
+		$db = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DBNAME, 3306) or die('Error: Unable to Connect');
 	$username = mysqli_real_escape_string($db, $_POST['username']);
 	$times = microtime(true);
 	$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
