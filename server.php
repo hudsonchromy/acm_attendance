88c01dd780a9ded480c36b051a602677659088c8
hudsonchromy <hjc0018@auburn.edu>
2019-09-04 11:51:38 -0500

    coolio

--- server.php
+++ server.php
@@ -11,15 +11,17 @@ if (isset($_POST['login'])) {
 	if ($_SESSION['event'] == "-- Select Event --") {
 		header("location: event.php");
 	}
-	$club = $_SESSION['event'];
-	$HOST = getenv('host');
-	$USERNAME = getenv('username');
-	$PASSWORD = getenv('password');
-	$DBNAME = getenv('dbname');
-	$db = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DBNAME) or die('Error: Unable to Connect');
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$times = microtime(true);
-	$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
-	$result = mysqli_query($db, $sql);
+	else {
+		$club = $_SESSION['event'];
+		$HOST = getenv('host');
+		$USERNAME = getenv('username');
+		$PASSWORD = getenv('password');
+		$DBNAME = getenv('dbname');
+		$db = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DBNAME) or die('Error: Unable to Connect');
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$times = microtime(true);
+		$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
+		$result = mysqli_query($db, $sql);
+	}
 }
 ?>
