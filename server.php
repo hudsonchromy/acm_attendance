23fb4a6ad6aaf9f4cc91fa038166e1a58053b851
hudsonchromy <hjc0018@auburn.edu>
2019-09-05 09:57:23 -0500

    coolio

--- server.php
+++ server.php
@@ -18,10 +18,19 @@ if (isset($_POST['login'])) {
 		$PASSWORD = getenv('password');
 		$DBNAME = getenv('dbname');
 		$db = mysqli_connect($HOST, $USERNAME, $PASSWORD, $DBNAME) or die('Error: Unable to Connect');
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$times = microtime(true);
-		$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
-		$result = mysqli_query($db, $sql);
+		$username = strtolower(mysqli_real_escape_string($db, $_POST['username']));
+
+		$i = 0;
+		$curr;
+		$good = true;
+		if (ctype_alpha(substr($username, 0, 3)) && ctype_digit(substr($username, 3, 4))) {
+			$times = microtime(true);
+			$sql = "INSERT INTO checkins (username, timems, event) VALUES ('$username', $times, '$club')";
+			$result = mysqli_query($db, $sql);
+		}
+		else {
+			echo "Error";
+		}
 	}
 }
 ?>
